43ee0cf0595fd9ff93335bbe32eb273609e4cc66
Ansix1207 <choiansik98@naver.com>
2022-10-07 05:38:49 +0900

    #6 spring study on MAC  - 고객 관리 시스템 : 고객 목록 및 작성 Bootstrap 적용  - 고객 관리 시스템 : 정보 수정 Bootstrap 적용

--- src/main/java/com/example/springbookstudy/web/CustomerController.java
+++ src/main/java/com/example/springbookstudy/web/CustomerController.java
@@ -2,6 +2,7 @@ package com.example.springbookstudy.web;
 
 import com.example.springbookstudy.domain.Customer;
 import com.example.springbookstudy.service.CustomerService;
+import org.apache.coyote.Response;
 import org.apache.juli.logging.Log;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.PropertyEditorRegistrar;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -27,7 +29,6 @@ public class CustomerController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-//    @GetMapping
     String list(Model model) {
         List<Customer> customers = customerService.findAll();
         model.addAttribute("customers", customers);
@@ -45,28 +46,50 @@ public class CustomerController {
         return "redirect:/customers";
     }
 
-    @RequestMapping(value = "edit", params = "form", method = RequestMethod.GET)
+    /*Todo : ReqeustMapping 이해하기, value params에 대해서
+        value가 붙으면 URI 에서 매핑시에 해당하는 주소로 매핑을 시킨다.
+          params는 그 뒤, ~~~/customers/edit?[params]=(html에서 해당 name의 value 값)
+          에 해당하는 값들이 들어가진다. 예시 : ~~~/customers/edit?form=편집
+          그 다음 매개변수(Parameter) 위치에 써져있는것들 중 @RequestParam을 적으면
+          자바코드 내에서 변수로 사용이 가능해지고 URI 의 쿼리문뒤에 붙게됨.
+          예시 : ~~~/customers/edit?form=편집&id=1(customer.id)*/
+    @RequestMapping(value = "edit", /*params = "form",*/ method = RequestMethod.GET)
     String editForm(@RequestParam Integer id, CustomerForm form) {
         Customer customer = customerService.findOne(id);
         BeanUtils.copyProperties(customer, form);
+        System.out.println("edit Form...");
         return "customers/edit";
     }
 
     @RequestMapping(value = "edit", method = RequestMethod.POST)
     String edit(@RequestParam Integer id, @Validated CustomerForm form
-            , BindingResult result) {
+            , BindingResult result, RedirectAttributes rt) {
+        //RedirectAttributes 를 사용하여 1회성으로 리다이렉트시 alert사용을 위한 msg 전송.
         if (result.hasErrors()) {
+            System.out.println("edit if문...");
             return editForm(id, form);
         }
+        System.out.println("edit...");
         Customer customer = new Customer();
         BeanUtils.copyProperties(form, customer);
         customer.setId(id);
         customerService.update(customer);
-        return "redirect:/customer";
+        String msg = "성공적으로 변경되었습니다.";
+        rt.addFlashAttribute("msg", msg);
+        return "redirect:/customers";
     }
 
     @RequestMapping(value = "edit", params = "goToTop")
     String goToTop(){
+        return "redirect:/customers";
+    }
+
+    @RequestMapping(value = "delete",method = RequestMethod.POST)
+    String delete(@RequestParam Integer id, RedirectAttributes rt){
+        System.out.println("[CONTROLLER]delete : id = " + id);
+        customerService.delete(id);
+        String msg = "성공적으로 삭제되었습니다.";
+        rt.addFlashAttribute("msg", msg);
         return "redirect:/customers";
     }
 }
